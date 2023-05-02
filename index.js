3d183ae282382dc820e00a35173ee44ef9e0eeb6
Hugo Virgen <virgenherrera@gmail.com>
2023-05-02 13:32:28 -0600

    fix install error

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { join } = require('path');
-const { projects } = require('./angular.json');
-const [projectName] = Object.keys(projects);
-const {
-  architect: {
-    build: {
-      options: { outputPath },
-    },
-  },
-} = projects[projectName];
-const builtHtmlPath = join(__dirname, outputPath);
 
-module.exports.builtHtmlPath = builtHtmlPath;
+module.exports.publicHtmlPath = join(__dirname, 'public_html');
